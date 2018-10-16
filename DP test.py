637b1bc16b3c262d61760e62fc1fef38309cf376
Petter Eikeland <paeikela@stud.ntnu.no>
2018-10-16 18:37:50 +0200

    Added dd-model branch under features

---
+++ DP test.py
@@ -0,0 +1,21 @@
+import math
+import pandas as pd
+
+
+
+def test(a,b,c,d,e,f):
+    print(a,b,c,d,e,f)
+
+
+
+
+if __name__ == '__main__':
+    e_bh = [1,2]
+    e_bm = [1,2]
+    e_mb = [1,2]
+    e_mh = [1,2]
+    k_u = [0,1]
+    k_d = [0,1]
+
+
+    test(e_bh,e_bm,e_mb,e_mh,k_u,k_d)
